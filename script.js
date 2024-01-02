6491782aa1fc33454a6be5300a5f0255181e981a
sebe <sebe2k04@gmail.com>
2024-01-02 21:52:38 +0530

    done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var input=document.querySelector("#input");
 var div=document.querySelector(".list")
 function add(){
     var list=document.createElement("li");
-    list.innerHTML=input.value+'<button onclick="del(event)" id="del">delete</button>';
+    list.innerHTML=input.value+'<button onclick="del(event)" id="del">Delete</button>';
     div.append(list);
 }
 function del(event){
